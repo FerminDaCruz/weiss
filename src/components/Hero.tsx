3f22d5b1c95b20f7fb642106929d27f0cd67424f
Ferminju <ferminjuliancruz26@gmail.com>
2025-08-12 14:37:20 -0300

    nombre responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,5 @@ import burgers from "@/data/burgers.json";
 
 export default function Hero() {
     return (
-        <>
-            <h1>Hero con hamburguesa 3D</h1>
-            <div>
-                {burgers.map((burger) => (
-                    <Burger3D key={burger.id} burger={burger} />
-                ))}
-            </div>
-        </>
     );
 }
